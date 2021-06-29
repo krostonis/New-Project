33f9a847cf37be5431e816a8c7d9c6ff3a2a5a57
Maksim Bobrov <krostonis@gmail.com>
2021-06-29 13:14:56 +0300

    New Jobs

---
+++ new project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NewProject
+{
+    class Inches
+    {
+        static void Main(string [] args)
+        {
+            long inches;
+            long meters;
+
+            meters = 400;
+            inches = meters * 100 * 40;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("inches: {0}; meters: {1}", inches, meters);
+            
+            long kilogram;
+            long gram;
+            kilogram = 5;
+            gram = kilogram * 5 * 1000;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("kilogram: {0}; gram: {1}", kilogram, gram);
+            string str1 = new string("E.S.N.Channel");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            string str2 = "New Channel";
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+    }
+}
